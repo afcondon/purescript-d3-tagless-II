06be661eee4097e0568a9e54be53dec75659283b
Andrew Condon <andrew.condon@gmail.com>
2021-03-26 18:30:43 +0100

    GUP skeleton complete(ish) for Final Tagless
    console log looks good but the real test is to hook it up to D3 and see if it works
    
    lots of issues with data, transitions etc

--- src/Selection.js
+++ src/Selection.js
@@ -1,22 +1,47 @@
-exports.nullSelectionJS = null
+exports.nullD3Selection = null
 
-// d3SelectAll_ :: Selector -> SelectionJS
-exports.d3SelectAll_ = function(selector) {
-  console.log(`\td3SelectAll: ${selector}`);
+// d3SelectAll_ :: Selector -> D3Selection
+exports.d3SelectAllInDOM_ = selector => {
+  console.log(`\td3SelectAllInDOM: ${selector}`);
   return "rootSelection";
+  // return d3.selectAll(selector);
 } 
-// d3Append_ =    :: String -> SelectionJS -> SelectionJS
+// d3SelectAll_ :: Selector -> D3Selection
+exports.d3SelectionSelectAll_ = selector => selection => {
+  console.log(`\td3SelectionSelectAll: ${selection}.selectAll(${selector})`);
+  return "JoinSelection";
+  // return selection.selectAll(selector);
+} 
+// d3Enter_ :: D3Selection -> D3Selection
+exports.d3EnterAndAppend_ = element => selection => {
+  console.log(`\td3EnterAndAppend: ${selection}.enter().append(${element})`);
+  return "EnterSelection"
+  // return selection.enter().append(element);
+}
+// d3Exit_ :: D3Selection -> D3Selection
+exports.d3Exit_ = selection => {
+  console.log(`\td3Exit: ${selection}.exit()`);
+  return "exit selection"
+  // return selection.exit();
+}
+// d3RemoveSelection_ :: D3Selection -> D3Selection
+exports.d3RemoveSelection_ = selection => {
+  console.log(`\td3Remove: ${selection}.remove()`);
+  return "removedSelection"
+  // return selection.remove()
+}
+// d3Append_ :: String -> D3Selection -> D3Selection
 exports.d3Append_ = element => selection => {
   console.log(`\td3Append: ${selection}.append(${element})`);
   return "appendedSelection";
+  // return selection.append(selector);
 }
-// d3Join_ =      :: String -> SelectionJS -> SelectionJS
-exports.d3Join_ = element => selection => {
-  console.log(`\td3Join: ${element} to ${selection}`);
-  return "joined";
+// d3Data_ :: D3Data -> D3Selection -> D3Selection
+exports.d3Data_ = data => selection => {
+  console.log(`\td3Data: ${selection}.data(${data})`);
+  return "dataBoundSelection"
 }
-
-// d3SetAttr_      :: String -> D3Attr -> SelectionJS -> Unit 
+// d3SetAttr_      :: String -> D3Attr -> D3Selection -> Unit 
 exports.d3SetAttr_ = name => value => selection => {
   console.log(`\t${selection}.attr(${name}, ${value})`);
   // selection.attr(name, value);
--- src/Selection.purs
+++ src/Selection.purs
@@ -1,9 +1,9 @@
 module Selection where
 
-import Prelude hiding (append, join)
+import Prelude hiding (append,join)
 
-import Attributes.Instances (Attribute(..), Attributes, Datum, unbox)
 import Attributes.Helpers (fill, strokeColor, strokeOpacity)
+import Attributes.Instances (Attribute(..), Attributes, Datum, unbox)
 import Control.Monad.State (class MonadState, StateT, get, put, runStateT)
 import Data.Tuple (Tuple, fst, snd)
 import Effect (Effect)
@@ -23,8 +23,29 @@ instance showElement :: Show Element where
   show Group  = "group"
   
 -- || trying this with Finally Tagless instead of interpreter
-foreign import data SelectionJS :: Type
-foreign import nullSelectionJS :: SelectionJS -- probably just null
+foreign import data D3Selection :: Type
+-- we'll coerce everything to this type if we can validate attr lambdas against provided data
+foreign import data D3Data :: Type 
+-- ... and we'll also just coerce all our setters to one thing for the FFI since JS don't care
+foreign import data D3Attr :: Type 
+foreign import nullD3Selection :: D3Selection -- probably just null
+
+foreign import d3SelectAllInDOM_     :: Selector -> D3Selection
+foreign import d3SelectionSelectAll_ :: Selector -> D3Selection -> D3Selection
+foreign import d3EnterAndAppend_     :: String   -> D3Selection -> D3Selection
+foreign import d3Append_             :: String   -> D3Selection -> D3Selection
+foreign import d3Exit_               :: D3Selection -> D3Selection
+foreign import d3Data_               :: D3Data   -> D3Selection -> D3Selection
+foreign import d3RemoveSelection_    :: D3Selection -> D3Selection
+
+-- NB D3 returns the selection after setting an Attr but we will only capture Selections that are 
+-- meaningfully different _as_ selections, we're not chaining them in the same way
+-- foreign import d3GetAttr_ :: String -> D3Selection -> ???? -- solve the ???? as needed later
+foreign import d3SetAttr_      :: String -> D3Attr -> D3Selection -> Unit
+
+coerceD3Data :: forall a. a -> D3Data
+coerceD3Data = unsafeCoerce
+
 
 data Node = Node Element Attributes (Array Node)
 
@@ -44,38 +65,38 @@ testEnterUpdateExit :: { enter :: Node
 testEnterUpdateExit = { enter: node__ Line, update: node__ Circle, exit: node__ Group }
 
 type EnterUpdateExit = {
-    enter  :: Node
-  , update :: Node
-  , exit   :: Node
+    enter  :: Attributes -- BUT, these might become [ Tuple Attributes Transition ]
+  , update :: Attributes -- BUT, these might become [ Tuple Attributes Transition ]
+  , exit   :: Attributes -- BUT, these might become [ Tuple Attributes Transition ]
 }
 
-newtype D3M a = D3M (StateT SelectionJS Effect a) -- not using Effect to keep sigs simple for now
+newtype D3M a = D3M (StateT D3Selection Effect a) -- not using Effect to keep sigs simple for now
 
 derive newtype instance functorD3M     :: Functor           D3M
 derive newtype instance applyD3M       :: Apply             D3M
 derive newtype instance applicativeD3M :: Applicative       D3M
 derive newtype instance bindD3M        :: Bind              D3M
 derive newtype instance monadD3M       :: Monad             D3M
-derive newtype instance monadStateD3M  :: MonadState SelectionJS D3M
+derive newtype instance monadStateD3M  :: MonadState D3Selection D3M
 derive newtype instance monadEffD3M    :: MonadEffect       D3M
 
 class (Monad m) <= D3Tagless m where
-  hook   :: Selector -> m SelectionJS
-  append :: Node -> m SelectionJS
-  join   :: Element -> EnterUpdateExit -> m SelectionJS
+  hook   :: Selector -> m D3Selection
+  append :: Node -> m D3Selection
+  join   :: Element -> EnterUpdateExit -> m D3Selection
 
-runD3M :: ∀ a. D3M a -> SelectionJS -> Effect (Tuple a SelectionJS)
+runD3M :: ∀ a. D3M a -> D3Selection -> Effect (Tuple a D3Selection)
 runD3M (D3M selection) = runStateT selection
 
-d3State :: ∀ a. D3M a -> Effect SelectionJS
-d3State (D3M selection) = liftA1 snd $ runStateT selection nullSelectionJS
+d3State :: ∀ a. D3M a -> Effect D3Selection
+d3State (D3M selection) = liftA1 snd $ runStateT selection nullD3Selection
 
 d3Run :: ∀ a. D3M a -> Effect a
-d3Run (D3M fse) = liftA1 fst $ runStateT fse nullSelectionJS
+d3Run (D3M fse) = liftA1 fst $ runStateT fse nullD3Selection
 
 instance d3TaglessD3M :: D3Tagless D3M where
   hook selector = do
-    let selection = d3SelectAll_ selector
+    let selection = d3SelectAllInDOM_ selector
     put selection
     pure selection  -- no attributes or children on hook point (at least for now, KISS)
 
@@ -85,18 +106,27 @@ instance d3TaglessD3M :: D3Tagless D3M where
     put appended
     pure appended 
 
-  join element enterUpdateExit = do
+  join element enterUpdateExit = do -- TODO add data to the join
     selection <- get
-    let joined = d3Join_ (show element) selection
-        enter  = doAppend enterUpdateExit.enter joined
-        update = doAppend enterUpdateExit.update joined -- won't use doAppend with update and exit, i think
-        -- plus we might want to handle children differently for all three cases because transitions, attrs, sequencing
-        exit   = doAppend enterUpdateExit.exit joined -- then add the remove() call to exit??
-        -- call merge now with enter + update? i think so
-    put joined
-    pure joined
+    let -- TOOD d3Data_ with data
+        initialS = d3SelectionSelectAll_ (show element) selection
+        updateS  = d3Data_ (coerceD3Data [1,2,3]) initialS -- TODO this is where it's really tricky - attribute processing with shape of data open
+        _ = (setAttributeOnSelection updateS) <$> enterUpdateExit.update
+        -- TODO process further Tuple Transition Attributes things from enterUpdateExit.update
 
-doAppend :: Node -> SelectionJS -> SelectionJS
+        enterS  = d3Append_ (show element) updateS -- TODO add Attrs for the inserted element here
+        _      = (setAttributeOnSelection enterS) <$> enterUpdateExit.enter
+        -- TODO process further Tuple Transition Attributes things from enterUpdateExit.enter
+
+        exitS   = d3Exit_ updateS
+        _       = (setAttributeOnSelection exitS) <$> enterUpdateExit.exit
+        _       = d3RemoveSelection_ exitS
+
+    put updateS -- not clear to me what actually has to be returned from 
+    pure updateS
+
+
+doAppend :: Node -> D3Selection -> D3Selection
 doAppend (Node element attributes children) selection = do
   let appended = d3Append_ (show element) selection
       _ = d3SetAttr_ "x" (unsafeCoerce "foo") appended
@@ -105,7 +135,7 @@ doAppend (Node element attributes children) selection = do
   appended
 
 
-addChildToExisting :: SelectionJS -> Node -> SelectionJS
+addChildToExisting :: D3Selection -> Node -> D3Selection
 addChildToExisting selection (Node element attributes children) = do
     let appended = d3Append_ (show element) selection
         _ = d3SetAttr_ "x" (unsafeCoerce "baar") appended
@@ -113,23 +143,11 @@ addChildToExisting selection (Node element attributes children) = do
     appended
 
 
-setAttributeOnSelection :: SelectionJS -> Attribute -> Unit
+setAttributeOnSelection :: D3Selection -> Attribute -> Unit
 setAttributeOnSelection selection (Attribute label attr) = d3SetAttr_ label (unbox attr) selection
 
-appendChildToSelection :: SelectionJS -> Node -> SelectionJS
+appendChildToSelection :: D3Selection -> Node -> D3Selection
 appendChildToSelection selection (Node element attributes children)  = d3Append_ (show element) selection
-
-foreign import d3SelectAll_ :: Selector -> SelectionJS
-foreign import d3Append_    :: String   -> SelectionJS -> SelectionJS
-foreign import d3Join_      :: String   -> SelectionJS -> SelectionJS
-
--- NB D3 returns the selection after setting an Attr but we will only capture Selections that are 
--- meaningfully different _as_ selections, we're not chaining them in the same way
--- foreign import d3GetAttr_ :: String -> SelectionJS -> ???? -- solve the ???? as needed later
-foreign import d3SetAttr_      :: String -> D3Attr -> SelectionJS -> Unit 
-
--- we'll just coerce all our setters to one thing for the FFI since JS don't care
-foreign import data D3Attr :: Type 
 
 type SomeDatum = forall r. { fillColorField :: String | r }
 _SomeDatum :: Proxy SomeDatum
@@ -152,19 +170,19 @@ someAttributes _ = [
   , fill $ coerceFromSomeDatum (\d -> d.fillColorField)
 ]
 
--- linksGroup :: ∀ m. (D3Tagless m) => m SelectionJS
+-- linksGroup :: ∀ m. (D3Tagless m) => m D3Selection
 -- linksGroup = do
 --   _ <- sequence append [ Node { element: Group, attributes: [] } ]
 
 infixl 1 appendChildren as ++
 
-script :: ∀ m. (D3Tagless m) => m SelectionJS
+script :: ∀ m. (D3Tagless m) => m D3Selection
 script = do
     root <- hook "div#root"
     
     svg <- append $ Node Svg (someAttributes _SomeDatum ) [ Node Group [] [ node__ Circle ] ]
 
-    _ <- join Circle testEnterUpdateExit 
+    _ <- join Circle { enter: [], update: [], exit: [] }
 
-    pure nullSelectionJS
+    pure nullD3Selection
 
