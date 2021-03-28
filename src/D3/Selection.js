e54bd04f88af5af94a9d231743858e654580f229
Andrew Condon <andrew.condon@gmail.com>
2021-03-28 10:01:21 +0200

    mostly harmless
    no crashes in D3 but no actual side-effects from script either!

--- src/D3/Selection.js
+++ src/D3/Selection.js
@@ -3,26 +3,26 @@ exports.nullD3Selection = null
 // d3SelectAll_ :: Selector -> D3Selection
 exports.d3SelectAllInDOM_ = selector => {
   console.log(`\td3SelectAllInDOM: ${selector}`);
-  return "rootSelection";
-  // return d3.selectAll(selector);
+  // return "rootSelection";
+  return d3.selectAll(selector);
 } 
 // d3SelectAll_ :: Selector -> D3Selection
 exports.d3SelectionSelectAll_ = selector => selection => {
   console.log(`\td3SelectionSelectAll: ${selection}.selectAll(${selector})`);
-  return "JoinSelection";
-  // return selection.selectAll(selector);
+  // return "JoinSelection";
+  return selection.selectAll(selector);
 } 
 // d3Enter_ :: D3Selection -> D3Selection
 exports.d3EnterAndAppend_ = element => selection => {
   console.log(`\td3EnterAndAppend: ${selection}.enter().append(${element})`);
-  return "EnterSelection"
-  // return selection.enter().append(element);
+  // return "EnterSelection"
+  return selection.enter().append(element);
 }
 // d3Exit_ :: D3Selection -> D3Selection
 exports.d3Exit_ = selection => {
   console.log(`\td3Exit: ${selection}.exit()`);
-  return "exit selection"
-  // return selection.exit();
+  // return "exit selection"
+  return selection.exit();
 }
 // d3AddTransition :: D3Selection -> D3Selection
 exports.d3AddTransition = selection => transition => {
@@ -48,24 +48,24 @@ exports.d3AddTransition = selection => transition => {
 // d3RemoveSelection_ :: D3Selection -> D3Selection
 exports.d3RemoveSelection_ = selection => {
   console.log(`\td3Remove: ${selection}.remove()`);
-  return "removedSelection"
-  // return selection.remove()
+  // return "removedSelection"
+  return selection.remove()
 }
 // d3Append_ :: String -> D3Selection -> D3Selection
 exports.d3Append_ = element => selection => {
   console.log(`\td3Append: ${selection}.append(${element})`);
-  return "appendedSelection";
-  // return selection.append(selector);
+  // return "appendedSelection";
+  return selection.append(element);
 }
 // d3Data_ :: D3Data -> D3Selection -> D3Selection
 exports.d3Data_ = data => selection => {
   console.log(`\td3Data: ${selection}.data(${data})`);
-  return "dataBoundSelection"
+  // return "dataBoundSelection"
+  return selection.data(data);
 }
 // d3SetAttr_      :: String -> D3Attr -> D3Selection -> Unit 
 exports.d3SetAttr_ = name => value => selection => {
   console.log(`\t${selection}.attr(${name}, ${value})`);
-  // selection.attr(name, value);
-  return `attrAdded`
+  // return `attrAdded`
+  selection.attr(name, value);
 }
-
