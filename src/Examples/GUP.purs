b74e6e9beee19400ac440ead51d9baf9f95423e2
Andrew Condon <andrew.condon@gmail.com>
2021-03-29 10:45:43 +0200

    reference commit - illustrative error case
    in D3 there’s nothing to stop you putting a computed attribute on a part of the DOM that hasn’t had a data join such as the SVG here…need to think how to guard against this

--- src/Examples/GUP.purs
+++ src/Examples/GUP.purs
@@ -15,19 +15,21 @@ import Unsafe.Coerce (unsafeCoerce)
 -- WIP all the attr functions are written in terms of Datum which is opaque, but we know at compile-time what they 
 -- really are so if we coerce the function AFTER we've type checked it against the type we know it will really be
 -- we should be able to have polymorphism AND type-checking
-type SomeDatum = Char
-_SomeDatum :: Proxy SomeDatum
-_SomeDatum = Proxy
+type CharDatum = Char
+_CharDatum :: Proxy CharDatum
+_CharDatum = Proxy
 
-someAttributes :: Attributes
-someAttributes = [
+someAttributes :: Proxy CharDatum -> Attributes
+someAttributes proxy = [
     strokeColor "green"
   , strokeOpacity 0.75
-  , strokeWidth (strokeWidthFn _SomeDatum)
+  , strokeWidth (strokeWidthFn proxy )
 ]
 
-strokeWidthFn :: Proxy SomeDatum -> Datum -> Number
-strokeWidthFn _ d = toNumber $ toCharCode (unsafeCoerce d :: SomeDatum)
+strokeWidthFn :: Proxy CharDatum -> Datum -> Number
+strokeWidthFn proxy d = toNumber $ 
+                        (toCharCode
+                        (unsafeCoerce d :: CharDatum)) - 97 
 
 
 type Model = Array Char
@@ -35,11 +37,11 @@ type Model = Array Char
 script :: ∀ m. (D3Tagless m) => m D3Selection
 script = do
   _    <- model [ 'a', 'b', 'c', 'd' ]
+
   root <- hook "div#root"
   
-  -- svg <- append $ D3_Node Svg (someAttributes _SomeDatum ) [ D3_Node Group [] [ node__ Circle ] ]
-  svg <- append $ D3_Node Svg someAttributes [ D3_Node Group [] [ node__ Circle ] ]
+  svg  <- append $ D3_Node Svg (someAttributes _CharDatum) [ D3_Node Group [] [ node__ Circle ] ]
 
-  _ <- join Circle { enter: [], update: [], exit: [] }
+  _    <- join Circle { enter: [], update: [], exit: [] }
 
   pure svg
