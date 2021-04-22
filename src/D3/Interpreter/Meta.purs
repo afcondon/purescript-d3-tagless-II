bf21a2b1e771a8a7f5014206520235b05ffbeeab
Andrew Condon <andrew.condon@gmail.com>
2021-04-22 13:24:33 +0200

    refactor helpers into state monad

--- src/D3/Interpreter/Meta.purs
+++ src/D3/Interpreter/Meta.purs
@@ -1,16 +1,18 @@
 module D3.Interpreter.MetaTree where
 
-import Control.Monad.State (class MonadState, StateT, get, modify_, runStateT)
+import Prelude
+
+import Control.Monad.State (class MonadState, StateT, State, get, modify_, runStateT)
 import D3.Attributes.Instances (Attribute(..), unbox)
 import D3.Interpreter (class D3InterpreterM)
 import D3.Layouts.Hierarchical (Tree(..))
 import D3.Selection (Chainable(..), D3_Node(..), Element, EnterUpdateExit, Join(..), Keys, ZoomConfig, Transition, showAddTransition_, showRemoveSelection_, showSetAttr_, showSetText_)
 import Data.Array (foldl)
 import Data.Map (Map, empty, insert)
+import Data.Traversable (traverse)
 import Data.Tuple (Tuple)
 import Effect (Effect)
 import Effect.Class (class MonadEffect)
-import Prelude (class Applicative, class Apply, class Bind, class Functor, class Monad, discard, bind, mempty, pure, (+))
 
 type NodeID = Int
 data MetaTreeNode = 
@@ -18,13 +20,13 @@ data MetaTreeNode =
   | AttachNode String
   | AppendNode Element
   -- TODO if datum type can be peeled off the Join type, just store the Join directly
-  | JoinSimpleNode     NodeID Element Keys (Array Chainable)
-  | JoinGeneralNode    NodeID Element Keys EnterUpdateExit
-  | JoinSimulationNode NodeID Element Keys (Array Chainable)
+  | JoinSimpleNode     Element Keys (Array Chainable)
+  | JoinGeneralNode    Element Keys EnterUpdateExit
+  | JoinSimulationNode Element Keys (Array Chainable)
   -- the next nodes are for nodes that are attributes and transitions and zooms which are all handled differently
   | ZoomNode ZoomConfig
   | AttrNode Chainable -- actually only Attr and Text
-  | TransitionNode Transition
+  | TransitionNode (Array Chainable) Transition
   | RemoveNode
 
 type MetaTree = Map NodeID MetaTreeNode
@@ -47,49 +49,49 @@ derive newtype instance monadD3MetaTreeM       :: Monad                 D3MetaTr
 derive newtype instance monadStateD3MetaTreeM  :: MonadState ScriptTree D3MetaTreeM 
 derive newtype instance monadEffD3MetaTreeM    :: MonadEffect           D3MetaTreeM
 
-insertInScriptTree :: ScriptTree -> MetaTreeNode -> ScriptTree
-insertInScriptTree (ScriptTree id nodeMap) newNode@(TransitionNode transition) = ScriptTree (id + 1) (insert id newNode nodeMap)
-insertInScriptTree (ScriptTree id nodeMap) newNode = ScriptTree (id + 1) (insert id newNode nodeMap)
+insertInScriptTree :: MetaTreeNode -> D3MetaTreeM NodeID
+-- returns the number of the next node
+insertInScriptTree transition@(TransitionNode chain config) = do
+  (ScriptTree id nodeMap) <- get
+  modify_ (\s -> ScriptTree (id + 1) (insert id transition nodeMap))
+  _ <- traverse insertAttributeInScriptTree chain
+  (ScriptTree nextId _) <- get
+  pure nextId
+insertInScriptTree newNode = do
+  (ScriptTree id nodeMap) <- get
+  modify_ (\s -> ScriptTree (id + 1) (insert id newNode nodeMap))
+  (ScriptTree nextId _) <- get
+  pure (id + 1)
 
-insertAttributeInScriptTree :: ScriptTree -> Chainable -> ScriptTree
-insertAttributeInScriptTree (ScriptTree id nodeMap) = 
-  case _ of -- the transition attribute is an exception, it can have further (Array Chainable)
-    attr@(AttrT _) -> ScriptTree (id + 1) (insert id (AttrNode attr) nodeMap)
-    text@(TextT _) -> ScriptTree (id + 1) (insert id (AttrNode text) nodeMap)
-    RemoveT        -> ScriptTree (id + 1) (insert id RemoveNode nodeMap)
-    transition@(TransitionT chain config) ->
-      insertInScriptTree (ScriptTree id nodeMap) (TransitionNode config) -- TODO recurse on the chain part
+insertAttributeInScriptTree :: Chainable -> D3MetaTreeM NodeID
+insertAttributeInScriptTree = 
+  case _ of 
+      -- simple attributes are just nodes
+      attr@(AttrT _) -> insertInScriptTree (AttrNode attr)
+      text@(TextT _) -> insertInScriptTree (AttrNode text)
+      RemoveT        -> insertInScriptTree RemoveNode
+-- the transition attribute is an exception, it can have further (Array Chainable)
+      transition@(TransitionT chain config) ->
+        insertInScriptTree (TransitionNode chain config)
+  
 
 
 instance d3Tagless :: D3InterpreterM NodeID D3MetaTreeM where
-  attach selector = do
-    (ScriptTree id _) <- get
-    modify_ (\s -> insertInScriptTree s (AttachNode selector))
-    pure (id + 1)
+  attach selector = insertInScriptTree (AttachNode selector)
 
   append selection (D3_Node element attributes) = do
-    (ScriptTree id _) <- get
-    modify_ (\s -> insertInScriptTree s (AppendNode element))
-    modify_ (\s -> foldl insertAttributeInScriptTree s attributes)
+    _ <- insertInScriptTree (AppendNode element)
+    _ <- traverse insertAttributeInScriptTree attributes
     (ScriptTree nextId _) <- get
     pure nextId
 
-  join selection theJoin = do
-    (ScriptTree id _) <- get
-    case theJoin of
-      (Join j) ->
-        modify_ (\s -> insertInScriptTree s (JoinSimpleNode id j.element j.key j.behaviour))
-      (JoinGeneral j) ->
-        modify_ (\s -> insertInScriptTree s (JoinGeneralNode id j.element j.key j.behaviour))
-      (JoinSimulation j) ->
-        modify_ (\s -> insertInScriptTree s (JoinSimulationNode id j.element j.key j.behaviour))
-    
-    pure (id + 1)
+  join selection =
+    case _ of
+      (Join j)           -> insertInScriptTree (JoinSimpleNode     j.element j.key j.behaviour)
+      (JoinGeneral j)    -> insertInScriptTree (JoinGeneralNode    j.element j.key j.behaviour)
+      (JoinSimulation j) -> insertInScriptTree (JoinSimulationNode j.element j.key j.behaviour)
 
-  attachZoom selection zoomConfig = do
-    (ScriptTree id _) <- get
-    modify_ (\s -> insertInScriptTree s (ZoomNode zoomConfig))
-    pure (id + 1)
+  attachZoom selection zoomConfig = pure 666 -- insertInScriptTree (ZoomNode zoomConfig)
 
 
 applyChainableString :: String -> Chainable -> String
