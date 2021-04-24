bd85bf22ce0f74aa429e76900432e5a604ee6cd3
Andrew Condon <andrew.condon@gmail.com>
2021-04-24 09:49:27 +0200

    cleaner presentation of main

--- src/Main.purs
+++ src/Main.purs
@@ -5,22 +5,29 @@ import Prelude
 import D3.Examples.Force as Graph
 import D3.Examples.GUP (runGeneralUpdatePattern) as GUP
 import D3.Examples.Tree.Radial as RadialTree
-import D3.Layouts.Hierarchical (getTreeViaAJAX, initRadialTree, makeModel)
-import Data.Either (Either(..))
-import Debug (spy)
+import D3.Layouts.Hierarchical (TreeJson_, getTreeViaAJAX, initRadialTree, makeModel)
+import Data.Bifunctor (rmap)
+import Data.Foldable (sequence_)
 import Effect (Effect)
-import Effect.Aff (forkAff, launchAff_)
+import Effect.Aff (Aff, forkAff, launchAff_)
+
+drawTree :: TreeJson_ -> Aff Unit
+drawTree json = RadialTree.drawTree =<< makeModel initRadialTree json
+
+drawMetaTree :: TreeJson_ -> Aff Unit
+drawMetaTree json = RadialTree.drawTree =<< makeModel initRadialTree =<< RadialTree.getMetaTreeJSON =<< makeModel initRadialTree json
+
 
 main :: Effect Unit
 main = launchAff_  do
   _        <- forkAff GUP.runGeneralUpdatePattern
   _        <- forkAff Graph.drawGraph
-  treeJSON <- getTreeViaAJAX "http://localhost:1234/flare-2.json"
 
-  _ <- case treeJSON of
-          (Left error) -> pure unit
-          (Right json) -> do
-                RadialTree.drawTree =<< makeModel initRadialTree json
-                RadialTree.drawTree =<< makeModel initRadialTree =<< RadialTree.getMetaTreeJSON =<< makeModel initRadialTree json
+  -- fetch an example model for the tree examples, the canonical flare dependency json in this case
+  treeJSON <- getTreeViaAJAX "http://localhost:1234/flare-2.json"
+  -- draw a radial tree using the flare data
+  sequence_ $ rmap drawTree treeJSON
+  -- draw a radial tree of the structure of the D3 "script" used to draw radial trees
+  sequence_ $ rmap drawMetaTree treeJSON
 
   pure unit
