6e86ee0f2b8b81f3e249320391851f1164edaa03
Andrew Condon <andrew.condon@gmail.com>
2021-06-03 09:47:20 +0200

    validate that forking the old main calls GUP

--- src/Main.purs
+++ src/Main.purs
@@ -2,40 +2,41 @@ module Main where
 
 import Prelude
 
+import D3.Data.Tree (TreeJson_, TreeLayout(..), TreeType(..))
+import D3.Examples.GUP (runGeneralUpdatePattern) as GUP
+import D3.Examples.LesMiserables as LesMis
+import D3.Examples.MetaTree as MetaTree
+import D3.Examples.Spago as Spago
+import D3.Examples.Tree.Configure as Tree
+import D3.Layouts.Hierarchical (getTreeViaAJAX, makeModel)
+import Data.Bifunctor (rmap)
+import Data.Foldable (sequence_)
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(Tuple))
 import Effect (Effect)
-import Stories.Count as ExpCount
-import Stories.GUP as D3GUP
-import Stories.Index as ExpIndex
-import Stories.Input as ExpInput
+import Effect.Aff (Aff, forkAff, launchAff, launchAff_)
+import Effect.Class.Console (log)
 import Foreign.Object as Object
 import Halogen.Aff as HA
 import Halogen.HTML as HH
 import Halogen.Storybook (Stories, runStorybook, proxy)
-import D3.Examples.LesMiserables as LesMis
-import D3.Examples.Spago as Spago
-import D3.Examples.GUP (runGeneralUpdatePattern) as GUP
-import D3.Examples.Tree.Configure as Tree
-import D3.Examples.MetaTree as MetaTree
-import D3.Layouts.Hierarchical (getTreeViaAJAX, makeModel)
-import D3.Data.Tree (TreeJson_, TreeLayout(..), TreeType(..))
-import Data.Bifunctor (rmap)
-import Data.Foldable (sequence_)
-import Effect.Aff (Aff, forkAff, launchAff_)
+import Stories.Count as ExpCount
+import Stories.GUP as D3GUP
+import Stories.Index as ExpIndex
+import Stories.Input as ExpInput
 
 drawMetaTree :: TreeJson_ -> Aff Unit
 drawMetaTree json =
   MetaTree.drawTree =<< makeModel TidyTree Vertical =<< Tree.getMetaTreeJSON =<< makeModel TidyTree Radial json
 
-ddi :: Effect Unit
-ddi = launchAff_  do
+ddi :: Aff Unit
+ddi = do
+  _        <- forkAff GUP.runGeneralUpdatePattern
+{-
   _        <- forkAff Spago.drawGraph
   
--- {-
   _        <- forkAff LesMis.drawGraph
 
-  _        <- forkAff GUP.runGeneralUpdatePattern
 
   -- fetch an example model for the tree examples, the canonical flare dependency json in this case
   treeJSON <- getTreeViaAJAX "http://localhost:1234/flare-2.json"
@@ -73,3 +74,4 @@ main = HA.runHalogenAff do
     { stories
     , logo: Just logo
     }
+  forkAff ddi
